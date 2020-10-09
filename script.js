79da647c3e657d472f514dc1b7be2f3e1b1e402c
designaky <designaky@gmail.com>
2020-10-09 18:16:20 +0100

    Very exited I have almost completed the carousel tasks, I just have to add the automatic loop

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const carol = document.querySelector(".images-carousel");
-const slides = Array.from(carol.children);
+// Images selection
+const carouselImages = document.querySelector(".images-carousel");
+const slides = Array.from(carouselImages.children);
+//controls selection
+const carouselCotntrols = Array.from(
+  document.querySelector(".carousel-controller").children
+);
 
-console.log(slides);
+const slidesLength = slides.length - 1;
+const imagesWith = carouselImages.clientWidth; // slide with so that you to move the images that you use css
+
+console.log(carouselCotntrols[0].id);
 
 const setSlidePosition = (slide, num) => {
   if (slide.style.transform) {
@@ -11,9 +19,9 @@ const setSlidePosition = (slide, num) => {
   }
   const newNum = num;
 
-  if (newNum <= 0 && newNum >= -600) {
+  if (newNum <= 0 && newNum >= -imagesWith * slidesLength) {
     slide.style.transform = `translateX(${newNum + "px"})`;
-    console.log(newNum);
+    //console.log(newNum);
   }
 };
 
@@ -25,12 +33,22 @@ const testIng = (num) => {
 
 const change = (event) => {
   return event.keyCode === 39
-    ? testIng(-300)
+    ? testIng(-imagesWith)
     : event.keyCode === 37
-    ? testIng(300)
+    ? testIng(imagesWith)
     : event.keyCode === 32
     ? testIng(32)
     : null;
 };
 
+// Event in the dom
+
 document.addEventListener("keydown", change);
+
+carouselCotntrols.forEach((el) => {
+  // For each element ot the Controls I have add the function tho change the images
+  el.addEventListener("click", (e) => {
+    if (el.id === "prev") testIng(imagesWith);
+    if (el.id === "next") testIng(-imagesWith);
+  });
+});
