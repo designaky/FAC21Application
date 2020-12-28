acdb2872e6ae54c9ac67b74540c1fa677e59c549
designaky <designaky@gmail.com>
2020-12-28 12:00:05 +0000

    Add Click buttons and section on the html

--- script2.js
+++ script2.js
@@ -1,13 +1,41 @@
-// On and off button
+// Elements selection
 const onButton = document.getElementById('on-button');
-
 const screen = document.querySelector('.screen');
+const vhsContainer = document.querySelectorAll('.vhs');
+const vhsIn = document.querySelector('.vhs-in h2');
+const channelType = document.querySelector('.channel-type');
 
+
+
+
+
+
+
+// On and off button
 const onOff = ()=>{
-    console.log('hello', onButton.classList)
     onButton.classList.toggle('off-button');
     onButton.classList.toggle('on-button');
     screen.classList.toggle('screen-on')
 };
 
 onButton.addEventListener('click', onOff);
+
+
+
+// Chose VHS 
+const changeVhs = (event)=>{
+    let vhsText = event.target.textContent; // seleccting the inner text for the changing
+    // Changin the VHS
+    vhsIn.textContent = vhsText;
+    // Changing the Channel Type
+    channelType.textContent = vhsText;
+    // Change the Content in the TV
+
+};
+
+
+vhsContainer.forEach(vhs => {
+    vhs.addEventListener('click', (event) =>{
+        changeVhs(event);
+    });   
+});
