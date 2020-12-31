855046e67a3edcddcf5aca14f5f1d72527a9f212
designaky <designaky@gmail.com>
2020-12-31 18:18:24 +0000

    CSS and Sort Card Game

--- script2.js
+++ script2.js
@@ -8,8 +8,12 @@ const welcomeMessage = document.querySelector ('.welcome-message')
 const tvVhsContent = document.querySelectorAll('.content');
 
 
+const cards = document.querySelectorAll('.card');
+const newGameBtn = document.querySelector('.new-game-btn');
 
 
+
+let prevPage ='Home';//initial page
 
 
 
@@ -17,18 +21,19 @@ const tvVhsContent = document.querySelectorAll('.content');
 const onOff = ()=>{
     onButton.classList.toggle('off-button');
     onButton.classList.toggle('on-button');
-    welcomeMessage.classList.toggle('welcome-message-off')
+    welcomeMessage.classList.toggle('welcome-message-off');
     
-    
+  
     tvVhsContent.forEach(vhsContent =>{
         //Changing the active page off
         if(vhsContent.classList.length > 1){
             vhsContent.classList.toggle('content-active');
+            prevPage = vhsContent.id;
         };
-        if(onButton.classList.value == 'on-button' && vhsContent.id == 'Home'){
+        if(onButton.classList.value == 'on-button' && vhsContent.id == prevPage){
             vhsContent.classList.toggle('content-active');
+            vhsIn.textContent = vhsContent.id;
             //channelType.textContent = vhsContent.id;
-
         };
        
     });
@@ -73,7 +78,25 @@ vhsContainer.forEach(vhs => {
         //if the television is on
         if (onButton.classList.value == 'on-button') {
             changeVhs(event);
-        }
+        };
         
     });   
 });
+
+
+const cardShuffle = ()=>{
+    //console.log(cards);
+    cards.forEach(card =>{
+       
+       card.style.order = 1 * Math.floor(Math.random()*cards.length);
+     
+        //console.log(card.style.order);
+    });    
+};
+
+
+const newGame = ()=>{
+    cardShuffle();
+};
+
+newGameBtn.addEventListener('click', newGame);
