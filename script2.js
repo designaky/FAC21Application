ad8230e471e32750f2baaf4f2e0a6c5cacea1bfe
designaky <designaky@gmail.com>
2021-01-01 19:27:08 +0000

    Finally I have finished the game

--- script2.js
+++ script2.js
@@ -113,15 +113,29 @@ const gameOn = (event)=>{
     console.log(selectedCard, prevCard);
     if(prevCard == '0-0'){
         prevCard= event.target
+        prevCard.classList.toggle('cardCorrect')
     } else {
         if(prevCard.id.charAt(0) == selectedCard.id.charAt(0)){
             console.log('well done')
-            prevCard.classList.toggle('cardCorrect')
+            //prevCard.classList.toggle('cardCorrect')
             selectedCard.classList.toggle('cardCorrect')
             prevCard = '0-0'
             win++               
         } else {
+            prevCard.classList.toggle('cardCorrect')
+            prevCard.classList.toggle('cardWrong')
+            selectedCard.classList.toggle('cardWrong')
+           //wait 1 sec 
+           setTimeout(()=>{
+            prevCard.classList.toggle('cardWrong')
             prevCard = '0-0'
+           }, 1200)
+           setTimeout(()=>{
+            selectedCard.classList.toggle('cardWrong')
+           }, 1200)
+            
+            
+            
         }
     }
     if(win==3){      
@@ -133,6 +147,7 @@ const removeCardToggle = ()=>{
     cards.forEach(card =>{
         console.log(card.classList)
         card.classList.remove('cardCorrect')
+        card.classList.remove('cardWrong')
         card.style.display = 'grid'
         console.log(card.classList)
     });
